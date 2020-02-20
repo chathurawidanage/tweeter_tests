20c5f631292fb158d808e7a4dced63f4eeb314db
Chathura Widanage <chathurawidanage@gmail.com>
2020-02-20 10:59:50 -0500

    adding missing param

--- load_json_dump.sh
+++ load_json_dump.sh
@@ -1,2 +1,2 @@
 # this script can be used to load a json dump to mongodb
-time mongoimport --db Tweets --collection compliance --file 2020-02-16-compliance.json
+time mongoimport --db Tweets --collection compliance --file 2020-02-16-compliance.json --batchSize 1000
